73497217c0808f39dc1e54ea432745601d2edc49
KarthikrajanTP <karthikrajan@testpress.in>
2024-08-08 07:31:10 +0000

    Add image field to Dish model

--- restaurants/models.py
+++ restaurants/models.py
@@ -69,6 +69,7 @@ class Dish(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     dish_type = models.CharField(max_length=20, choices=DISH_TYPE_CHOICES)
+    image = models.ImageField(upload_to='dish_photos/', null=True, blank=True)
 
     def __str__(self):
         return self.name
